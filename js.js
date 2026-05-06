6ada034ad570d963d4ac39fba9a0be91b4bbaa94
Zosia <sonya.20.gan@gmail.com>
2026-05-06 20:54:59 +0300

    more sections

---
+++ js.js
@@ -0,0 +1,9 @@
+// document.getElementById('readMoreScroll').addEventListener('click', function(event) {
+//     event.preventDefault();
+    
+//     const slider = document.querySelector('.card-box');
+//     // Беремо ширину посилання .card-link + gap (40px)
+//     const cardWidth = document.querySelector('.card-link').offsetWidth + 40; 
+    
+//     slider.scrollLeft -= cardWidth;
+// });

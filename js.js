9edd487829200ed75945f7c1d45c152a7cdf5094
Zosia <sonya.20.gan@gmail.com>
2026-05-08 17:00:52 +0300

    fullbase

--- js.js
+++
@@ -1,9 +0,0 @@
-// document.getElementById('readMoreScroll').addEventListener('click', function(event) {
-//     event.preventDefault();
-    
-//     const slider = document.querySelector('.card-box');
-//     // Беремо ширину посилання .card-link + gap (40px)
-//     const cardWidth = document.querySelector('.card-link').offsetWidth + 40; 
-    
-//     slider.scrollLeft -= cardWidth;
-// });

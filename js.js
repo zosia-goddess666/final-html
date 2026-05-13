e2976436298e4278b419614b0b30e57ca8f5a45c
Zosia <sonya.20.gan@gmail.com>
2026-05-13 19:11:06 +0300

    donat popup added

---
+++ js.js
@@ -0,0 +1,10 @@
+function popUpOpen(){
+    const popUpEl = document.getElementById("popUp");
+    console.log(popUp);
+    popUp.classList.toggle("hide");
+}
+function artPopUpOpen(){
+    const artPopUpEl = document.getElementById("artPopUp");
+    console.log(artPopUp);
+    popUp.classList.toggle("hide");
+}
